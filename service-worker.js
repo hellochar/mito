5623d62ab8feccaf0b9ecbeae6d84f5f6d692c4c
Xiaohan Zhang <hellocharlien@hotmail.com>
2019-10-29 17:53:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mito/precache-manifest.3950c15405958ff39281cb5e47b232cd.js"
+  "/mito/precache-manifest.45841f2b7ec50250386cc699c596bf68.js"
 );
 
 self.addEventListener('message', (event) => {
