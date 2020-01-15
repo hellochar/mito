e2faa40e7073363d3efaad1b1776570799bfe56b
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-01-15 20:54:24 +0800

    Delete Root class

--- src/sketches/mito/game/tile/index.ts
+++ src/sketches/mito/game/tile/index.ts
@@ -6,10 +6,9 @@ import { Fruit } from "./fruit";
 import { GrowingCell } from "./growingCell";
 import { Leaf } from "./leaf";
 import { Rock } from "./rock";
-import { Root } from "./root";
 import { Soil } from "./soil";
 import { Tile } from "./tile";
 import { Transport } from "./transport";
 
 export { CellEffect, FreezeEffect } from "./cellEffect";
-export { Air, DeadCell, Fountain, Rock, Soil, Tile, Cell, Fruit, GrowingCell, Leaf, Root, Transport };
+export { Air, DeadCell, Fountain, Rock, Soil, Tile, Cell, Fruit, GrowingCell, Leaf, Transport };
--- src/sketches/mito/game/tile/root.ts
+++ src/sketches/mito/game/tile/root.ts
@@ -1,7 +1,3 @@
-import { Vector2 } from "three";
-import { Interactable } from "../interactable";
-import { World } from "../world";
-import { Cell, CellArgs } from "./cell";
 import Chromosome from "./chromosome";
 import { GeneInventory, GeneLiving, GeneSoilAbsorption } from "./genes";
 import { GeneObstacle } from "./genes/GeneObstacle";
@@ -13,12 +9,6 @@ export const chromosomeRoot = new Chromosome(
   GeneObstacle.level(0),
   GeneSoilAbsorption.level(2)
 );
-
-export class Root extends Cell implements Interactable {
-  constructor(pos: Vector2, world: World, args?: CellArgs) {
-    super(pos, world, cellTypeRoot, args);
-  }
-}
 
 export const cellTypeRoot: CellType = {
   name: "Root",
