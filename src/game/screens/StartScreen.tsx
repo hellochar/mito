0ad53ca6e42e9e4f42b07495e915916f6fddf68e
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-04-04 17:00:50 -0700

    add Discord link to start screen

--- src/game/screens/StartScreen.tsx
+++ src/game/screens/StartScreen.tsx
@@ -1,6 +1,7 @@
 import { Button } from "game/ui/common/Button";
 import Character from "game/ui/common/Character";
 import React, { useEffect } from "react";
+import { FaDiscord } from "react-icons/fa";
 import "./StartScreen.scss";
 
 const StartScreen: React.FC<{ onStart: () => void }> = React.memo(({ onStart }) => {
@@ -28,9 +29,15 @@ const StartScreen: React.FC<{ onStart: () => void }> = React.memo(({ onStart }) 
         Mito
         <Character size="small" className="mito-i" />
       </h1>
-      <Button color="green" onClick={onStart}>
-        Start Game
-      </Button>
+      <div className="bottom-area">
+        <Button color="green" onClick={onStart}>
+          Start Game
+        </Button>
+        <a className="discord" rel="noopener noreferrer" target="_blank" href="http://discord.gg/N8wWwPX">
+          <FaDiscord />
+        </a>
+      </div>
+      <div className="game-version">v0.8.0-dev</div>
     </div>
   );
 });
