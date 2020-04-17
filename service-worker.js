1d6667fe4cec1586ffdb0574641fa981dbbb21e9
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-04-16 17:34:06 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.08ea9921c1f75afb14054b185a168691.js"
+  "./precache-manifest.d2aadf37c6c5a6ba826584924eb582f2.js"
 );
 
 self.addEventListener('message', (event) => {
